13243a5a4549091fee653bb324461481eab27749
jeffrey-wentz <jeff.a.wentz@gmail.com>
2019-04-01 23:14:38 -0700

    trying to integrate classes and more DRY

--- main.py
+++ main.py
@@ -2,42 +2,20 @@ from tkinter import *
 
 
 class Calculator:
+    def user_action(self):
+        print(self.user_input)
+
     def __init__(self, master):
-        self.master = master
-        master.title("Jeff's Calculator")
-
-        self.menu_button = Button(master, text="Menu", command=self.menu)
-        self.menu_button.grid(column=0, row=0)
-
-        self.history_button = Button(master, text="History", command=self.show_history)
-        self.history_button.grid(column=3, row=0, sticky=E)
-
-        self.equation_label = Label(master, text="equation input will be here")
-        self.equation_label.grid(column=3, row=1, sticky=E)
-
-        # most recent string of chars entered go here until an operator has been selected.
-        self.user_input_label = Label(master, text="user input here")
-        self.user_input_label.grid(column=3, row=2, sticky=E)
-
-        self.seven_button = Button(master, text="7")
-        self.history_button.grid(column=0, row=3)
-
-        self.eight_button = Button(master, text="8")
-        self.history_button.grid(column=1, row=3)
-
-        self.nine_button = Button(master, text="9")
-        self.history_button.grid(column=2, row=3)
-
-        self.multiply_button = Button(master, text="*")
-        self.history_button.grid(column=3, row=3)
-
-    def menu(self):
-        pass
-
-    def show_history(self):
-        pass
+        # Constructor method
+        master.title('Calculator')
+        self.user_input = ""
+        self.entry = Entry(master)
+        self.entry.grid(row=0, column=0, columnspan=6, pady=3)
+        self.user_input += self.entry.get()
+        Label(master, text=self.user_input).grid(row=0, column=4, columnspan=2)
+        Button(master, text="print", width=10, command=self.user_action()).grid(row=4, column=4, columnspan=2)
 
 
 root = Tk()
-calculator_gui = Calculator(root)
+obj = Calculator(root)
 root.mainloop()
